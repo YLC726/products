abf6e033857f66b4af14b412c1744a0cf6d3cb1b
YLC726 <g2002726@gmail.com>
2023-05-10 21:15:41 +0800

    read file

--- products.py
+++ products.py
@@ -1,4 +1,13 @@
 products = []
+#讀取既有檔案
+with open ('products.csv','r',encoding='utf-8') as f:
+    for line in f:
+        if '商品,價格' in line
+            continue
+        name,price = line.strip().split(',')#去除換行符號(/n) #用逗點切割字串定義為name,price
+        products.append([name,price])
+print(products)
+#讓使用者輸入
 while True:
     name = input('名稱,輸入完請按q:')
     if name == 'q':
@@ -6,14 +15,13 @@ while True:
     price = input('價格:')
     products.append([name , price])
 print(products)
-
-
+#印出購買紀錄
 for p in products:
 	print(p)
 
 for p in products:
 	print(p[0], '的價格是', p[1])
-
+#寫入檔案
 with open ('products.csv','w',encoding='utf-8') as f:
     f.write('商品,價格\n')
     for p in products:
